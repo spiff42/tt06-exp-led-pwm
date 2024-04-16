b93524b303a1c78e89d24d8e03d7677f8c3e0b68
Mikkel Holm Olsen <spiff@spiff-thinkpad>
2024-04-16 21:17:35 +0200

    Fix some lint warnings

--- src/tt_um_spiff42_exp_led_pwm.v
+++ src/tt_um_spiff42_exp_led_pwm.v
@@ -8,12 +8,14 @@
 module tt_um_spiff42_exp_led_pwm (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] uio_in,   // IOs: Input path
     output wire [7:0] uio_out,  // IOs: Output path
     output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // will go high when the design is enabled
     input  wire       clk,      // clock
-    input  wire       rst_n     // reset_n - low to reset
+    input  wire       rst_n,    // reset_n - low to reset
+    /* verilator lint_off UNUSEDSIGNAL */
+    input  wire [7:0] uio_in,   // IOs: Input path
+    input  wire       ena       // will go high when the design is enabled
+    /* verilator lint_on UNUSEDSIGNAL */
   );
 
   wire [7:0] ramp;
@@ -32,18 +34,7 @@ module tt_um_spiff42_exp_led_pwm (
   pwm_channel pwmch_6 (.value(pwm_val[6]), .ramp(ramp), .ch(uo_out[6]));
   pwm_channel pwmch_7 (.value(pwm_val[7]), .ramp(ramp), .ch(uo_out[7]));
 
-/*
-  pwm_channel pwmch_0 (.value(ui_in), .ramp(ramp), .ch(uo_out[0]));
-  pwm_channel pwmch_1 (.value(ui_in ^ 8'h10), .ramp(ramp), .ch(uo_out[1]));
-  pwm_channel pwmch_2 (.value(ui_in ^ 8'h20), .ramp(ramp), .ch(uo_out[2]));
-  pwm_channel pwmch_3 (.value(ui_in ^ 8'h30), .ramp(ramp), .ch(uo_out[3]));
-  pwm_channel pwmch_4 (.value(ui_in ^ 8'h40), .ramp(ramp), .ch(uo_out[4]));
-  pwm_channel pwmch_5 (.value(ui_in ^ 8'h50), .ramp(ramp), .ch(uo_out[5]));
-  pwm_channel pwmch_6 (.value(ui_in ^ 8'h60), .ramp(ramp), .ch(uo_out[6]));
-  pwm_channel pwmch_7 (.value(ui_in ^ 8'h70), .ramp(ramp), .ch(uo_out[7]));
-*/
-
-  wire        i2c_rw;
+  //wire        i2c_rw;
   wire [7:0]  i2c_addr;
   wire        i2c_wen;
   wire [7:0]  i2c_wdata;
@@ -60,7 +51,7 @@ module tt_um_spiff42_exp_led_pwm (
     .scl(uio_in[2]),
 
     // application interface
-    .rw(i2c_rw),
+    //.rw(i2c_rw),
     .addr(i2c_addr),
     .wen(i2c_wen),
     .wdata(i2c_wdata),
